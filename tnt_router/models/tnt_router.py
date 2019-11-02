a17c9c05804ef72265490fdc7a0db3a5d56f7a1d
agarmash <forwebsmmm@gmail.com>
2019-11-02 20:25:28 +0200

    work tree, form, before widget map

---
+++ tnt_router/models/tnt_router.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class TntRouter(models.Model):
+    _name = 'tnt.router'
+    _description = 'Tnt Router'
+
+    name = fields.Char(string='Route name')
+    from_customer = fields.Many2one('res.partner', string='From customer',
+                                    default=lambda self: self.env.user.commercial_partner_id.id,
+                                    domain="[('is_company','=',1)]")
+    customers = fields.Many2many('res.partner', string='Customers',  domain="[('customer','=',1), ('parent_id','=',False)]")
+
+
+class TntRoutePoint(models.Model):
+    _name = 'tnt.route.point'
+    _description = 'Tnt Route Point'
+
+    route = fields.Many2one('tnt.router', string='Route')
+    sequence = fields.Integer(string='Sequence')
+    latitude = fields.Float(string='Latitude', digits=(16, 5))
+    longitude = fields.Float(string='Longitude', digits=(16, 5))
