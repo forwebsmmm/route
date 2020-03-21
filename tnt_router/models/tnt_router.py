cec7449795e1fe376421be0b567ba9d50e624443
agarmash <forwebsmmm@gmail.com>
2020-03-21 18:13:32 +0200

    test ok

--- tnt_router/models/tnt_router.py
+++ tnt_router/models/tnt_router.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2016-TODAY touch:n:track <https://tnt.pythonanywhere.com>
 # Part of tnt: Router addon for Odoo. See LICENSE file for full copyright and licensing details.
-#dfffdfsfsd
 
 import openrouteservice
 
